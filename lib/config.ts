884c2a654dcab21193ed97bdd368d7a445a7ab1a
GETpta <vlad2408b@gmail.com>
2026-06-05 11:09:36 +0500

    fix: correct bot username string in config

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 // Username телеграм-бота из BotFather, БЕЗ символа @.
 // Пример: если бот называется @MarathonSkillsBot, сюда пиши "MarathonSkillsBot".
-export const TELEGRAM_BOT_USERNAME = "8393023758:AAG5vSFdMeG-hU-UEfSmw2x4uV5pSb6MnJo";
+export const TELEGRAM_BOT_USERNAME = "@Exchunger_bot";
